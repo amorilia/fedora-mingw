3a7409c6b277a84afb0b08acfb92de65e0e39c31
Richard W.M. Jones <rjones@redhat.com>
2008-10-30 09:41:32 +0000

    Add a bandwidth limit to stop it killing my ADSL.

--- upload.sh
+++ upload.sh
@@ -4,8 +4,9 @@ PORT=22222
 SERVER=annexia.org
 USERNAME=rich
 REMOTEDIR=tmp/mingw/
+KBPS=25 ;# kilobytes per second
 
-rsync -av --delete \
+rsync -av --delete --bwlimit=$KBPS \
   ~/public_html/smock/yum/fedora-9 \
   -e "ssh -p $PORT" \
   $USERNAME@$SERVER:$REMOTEDIR
