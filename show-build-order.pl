3fc64c61bf36496c43797df73d2d9a3f888c2966
Richard W.M. Jones <rjones@redhat.com>
2008-11-13 16:10:10 +0000

    Re-enable building mingw32-curl package.

--- show-build-order.pl
+++ show-build-order.pl
@@ -60,7 +60,6 @@ sub main {
     delete $br{"mingw32-gdb"};
     delete $br{"mingw32-python"};
     delete $br{"mingw32-ocaml"};
-    delete $br{"mingw32-curl"};
     delete $br{"mingw32-pidgin"};
     delete $br{"mingw32-nspr"};
     delete $br{"mingw32-nss"};
