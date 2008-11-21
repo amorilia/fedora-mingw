05bcbccd0acfd9ff467190e54f44f5cf78bd0b66
Richard W.M. Jones <rjones@redhat.com>
2008-11-21 13:36:36 +0000

    fedora-9 -> fedora-10

--- upload.sh
+++ upload.sh
@@ -7,6 +7,6 @@ REMOTEDIR=tmp/mingw/
 KBPS=25 ;# kilobytes per second
 
 rsync -av --delete --bwlimit=$KBPS \
-  ~/public_html/smock/yum/fedora-9 \
+  ~/public_html/smock/yum/fedora-10 \
   -e "ssh -p $PORT" \
   $USERNAME@$SERVER:$REMOTEDIR
