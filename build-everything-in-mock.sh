5a01ca2bdc64416cb6bfa4090b6ab60b34c00987
Richard W.M. Jones <rjones@redhat.com>
2009-01-14 18:31:06 +0000

    Allow extra SRPMS to be specified.

--- build-everything-in-mock.sh
+++ build-everything-in-mock.sh
@@ -14,15 +14,22 @@ wix"
 
 rm -f */*.src.rpm
 
+# If any extra SRPMS need to be considered, list them here.  Otherwise
+# leave this empty.
+srpms=""
+
 for dir in *; do
     if ! echo "$nobuild" | grep -sq "^$dir\$"; then
 	if [ -d $dir -a -f $dir/*.spec ]; then
-	    (
-		cd $dir
-		rpmbuild -bs --define "_sourcedir $(pwd)" --define "_srcrpmdir $(pwd)" *.spec
-	    )
+		if ! rpmbuild -bs --define "_sourcedir $(pwd)/$dir" --define "_srcrpmdir $(pwd)/$dir" $dir/*.spec; then
+		  echo "Failed to create a source RPM in directory $dir."
+		  echo "If you want to ignore this directory, you should add"
+		  echo "it to the 'nobuild' list in this script."
+		  exit 1
+		fi
+		srpms="$srpms $(pwd)/$dir/*.src.rpm"
 	fi
     fi
 done
 
-smock/smock.pl --arch=i386 --arch=x86_64 --distro=fedora-10 */*.src.rpm
+smock/smock.pl --arch=i386 --arch=x86_64 --distro=fedora-10 $srpms
