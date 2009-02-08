1db85ae7f325d391fce973142797fb7073ac0942
Richard W.M. Jones <rjones@redhat.com>
2009-02-08 17:28:46 +0000

    Don't limit uploads.

--- upload.sh
+++ upload.sh
@@ -4,9 +4,9 @@ PORT=22222
 SERVER=annexia.org
 USERNAME=rich
 REMOTEDIR=tmp/mingw/
-KBPS=25 ;# kilobytes per second
+#BWLIMIT="--bwlimit=25" ;# kilobytes per second
 
-rsync -av --delete --bwlimit=$KBPS \
+rsync -av --delete $BWLIMIT \
   ~/public_html/smock/yum/{fedora-10,fedora-rawhide} \
   -e "ssh -p $PORT" \
   $USERNAME@$SERVER:$REMOTEDIR
