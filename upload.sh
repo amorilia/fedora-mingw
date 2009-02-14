8dd15143a0e537e7825578079cda0f36ff053375
Richard W.M. Jones <rjones@redhat.com>
2009-02-14 13:42:09 +0000

    New yum repository.

--- upload.sh
+++ upload.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -
 
-PORT=22222
-SERVER=annexia.org
+SERVER=web.merjis.com
 USERNAME=rich
-REMOTEDIR=tmp/mingw/
+REMOTEDIR=public_html/mingw/
 #BWLIMIT="--bwlimit=25" ;# kilobytes per second
 
 rsync -av --delete $BWLIMIT \
   ~/public_html/smock/yum/{fedora-10,fedora-rawhide} \
-  -e "ssh -p $PORT" \
   $USERNAME@$SERVER:$REMOTEDIR
